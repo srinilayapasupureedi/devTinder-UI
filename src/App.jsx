96c186d440e1937165ccc6401c3ed6736e573870
Srinilaya Pasupureddi <srinilayap@gmail.com>
2025-10-15 11:03:37 +0530

    tailwindCss&&DaisyUI&& NavBar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './index.css';
+import NavBar from './NavBar';
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+     <h1 className="text-center font-bold underline">Hello world </h1>
+    </>
+  )
+}
+
+export default App
